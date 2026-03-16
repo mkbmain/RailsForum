354dda5c2c8944897cb935307bba82167cbd2d20
mkbmain <michael.bourke@live.co.uk>
2026-03-16 14:07:04 +0000

    feat: add Bannable concern

---
+++ app/controllers/concerns/bannable.rb
@@ -0,0 +1,18 @@
+module Bannable
+  extend ActiveSupport::Concern
+
+  private
+
+  # Depends on require_login having already run (current_user is guaranteed non-nil).
+  def check_not_banned
+    checker = BanChecker.new(current_user)
+    if checker.banned?
+      flash[:alert] = "You are banned until #{checker.banned_until.strftime("%B %-d, %Y")}."
+      redirect_to ban_redirect_path
+    end
+  end
+
+  def ban_redirect_path
+    root_path
+  end
+end
