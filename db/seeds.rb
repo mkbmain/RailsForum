43699266ee17b7377117f8649e0f2d78d51bdbec
mkbmain <michael.bourke@live.co.uk>
2026-03-16 14:03:19 +0000

    feat: seed ban_reasons (Spam, Harassment, Against Guidelines)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ Provider.find_or_create_by!(id: 2) { |p| p.name = "microsoft" }
 Provider.find_or_create_by!(id: 3) { |p| p.name = "internal" }
 
 puts "Seeded #{Provider.count} providers"
+
+["Spam", "Harassment", "Against Guidelines"].each do |name|
+  BanReason.find_or_create_by!(name: name)
+end
+puts "Seeded #{BanReason.count} ban reasons"
