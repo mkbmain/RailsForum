f84044e7b49e7fb438c01745038803df87b96240
mkbmain <michael.bourke@live.co.uk>
2026-03-17 10:59:29 +0000

    feat: add nested bans routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     resources :replies, only: [:create, :destroy]
   end
 
+  resources :users, only: [] do
+    resources :bans, only: [:new, :create]
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
