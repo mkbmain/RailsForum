1f07b11a4c2373258f0a45529d3ab343b149b266
mkbmain <michael.bourke@live.co.uk>
2026-03-22 04:27:25 +0000

    test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,25 +2,25 @@ require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
   test "valid with id and name" do
-    cat = Category.new(id: 2, name: "Tech")
+    cat = Category.new(id: 2, name: "Tech", position: 5)
     assert cat.valid?
   end
 
   test "invalid without name" do
-    cat = Category.new(id: 2)
+    cat = Category.new(id: 2, position: 5)
     assert_not cat.valid?
     assert_includes cat.errors[:name], "can't be blank"
   end
 
   test "name must be unique" do
     # 'other' fixture (id:1, name:'Other') is already loaded via fixtures :all
-    dup = Category.new(id: 2, name: "Other")
+    dup = Category.new(id: 2, name: "Other", position: 5)
     assert_not dup.valid?
     assert_includes dup.errors.full_messages, "Name has already been taken"
   end
 
   test "name max 100 characters" do
-    cat = Category.new(id: 2, name: "a" * 101)
+    cat = Category.new(id: 2, name: "a" * 101, position: 5)
     assert_not cat.valid?
     assert_includes cat.errors[:name], "is too long (maximum is 100 characters)"
   end
