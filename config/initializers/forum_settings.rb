91a21fc5175d7d69cd19d0b085dfdbae4172a9bd
mkbmain <michael.bourke@live.co.uk>
2026-03-22 14:30:57 +0000

    feat: add SESSION_TIMEOUT_MINUTES config constant

--- config/initializers/forum_settings.rb
+++ config/initializers/forum_settings.rb
@@ -1,2 +1,3 @@
 # config/initializers/forum_settings.rb
-EDIT_WINDOW_SECONDS = ENV.fetch("EDIT_WINDOW_SECONDS", 3600).to_i
+EDIT_WINDOW_SECONDS     = ENV.fetch("EDIT_WINDOW_SECONDS", 3600).to_i
+SESSION_TIMEOUT_MINUTES = ENV.fetch("SESSION_TIMEOUT_MINUTES", 2880).to_i
