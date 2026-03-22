5ff39b770f6efcbb2ded445d0de37c52ca97d3fc
mkbmain <michael.bourke@live.co.uk>
2026-03-22 03:24:17 +0000

    feat: add default_scope and position validation to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
   has_many :posts
 
+  default_scope { order(:position) }
+
   validates :name, presence: true, length: { maximum: 100 }, uniqueness: true
+  validates :position, numericality: { only_integer: true, greater_than: 0 }
 end
