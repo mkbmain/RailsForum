2366e25a49a0b4cebaed34f5172544e167906b8d
mkbmain <michael.bourke@live.co.uk>
2026-03-22 14:36:33 +0000

    feat: implement idle session timeout in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   include Moderatable
   helper_method :current_user, :logged_in?, :can_moderate?
 
+  before_action :check_session_timeout, if: :logged_in?
+  after_action  :touch_session
+
   private
 
   def current_user
@@ -14,5 +17,35 @@ class ApplicationController < ActionController::Base
 
   def require_login
     redirect_to login_path, alert: "Please log in first." and return unless logged_in?
+  end
+
+  def session_timeout_minutes
+    SESSION_TIMEOUT_MINUTES
+  end
+
+  def check_session_timeout
+    return if session_timeout_minutes == 0
+
+    unless session[:last_active_at]
+      session[:last_active_at] = Time.current.to_i
+      return
+    end
+
+    if Time.current.to_i - session[:last_active_at] > session_timeout_minutes * 60
+      @current_user = nil
+      reset_session
+
+      if turbo_frame_request? || request.format.turbo_stream? || request.format.json?
+        return head :unauthorized
+      else
+        redirect_to login_path, alert: "Your session has expired. Please log in again."
+        return
+      end
+    end
+  end
+
+  def touch_session
+    return unless session[:user_id].present? && session_timeout_minutes > 0
+    session[:last_active_at] = Time.current.to_i
   end
 end
